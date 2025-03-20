34103c4ece9b73c6e6270707b75f1b558199b5c1
bertapasamontes <bertapasamontes@gmail.com>
2025-03-20 20:47:18 +0100

    small info porfile + variables del scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SmallInfoPorfileComponent } from "./components/atomos/small-info-porfile/small-info-porfile.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SmallInfoPorfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/atomos/small-info-porfile/small-info-porfile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-info-porfile',
+  imports: [],
+  templateUrl: './small-info-porfile.component.html',
+  styleUrl: './small-info-porfile.component.scss'
+})
+export class SmallInfoPorfileComponent {
+
+}
